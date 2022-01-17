52806207e9a005f0109f26ad96df0010304aaf06
alcas44 <cescmazariegos@gmail.com>
2022-01-17 11:49:23 -0600

    Gracias a Dios se logro el logout MCMA

--- tienda/compras/urls.py
+++ tienda/compras/urls.py
@@ -5,7 +5,7 @@ from compras import views
 
 urlpatterns = [
     path('',views.inicio,name="Inicio"),
-    path('salir/',views.salir,name="Salir"),
+    path('salir/',views.logout_user,name="Salir"),
     path('compras/',views.compras,name="Compras"),
     path('nuevo/',views.nuevo,name="Nuevo"),
     path('editar/<int:id>',views.editar,name="Editar"),
--- tienda/compras/views.py
+++ tienda/compras/views.py
@@ -1,3 +1,5 @@
+import re
+#from telnetlib import AUTHENTICATION
 from django.contrib import messages
 from django.forms import forms
 from django.shortcuts import redirect, render
@@ -7,7 +9,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-   
+
 def inicio(request):
      if request.method == "POST":
          u = request.POST["user"]
@@ -18,75 +20,81 @@ def inicio(request):
              return redirect('Compras')
              
          else:
-             return redirect('Compras')    
-     
-
-     return render(request, 'compras/index.html')  
+            return redirect('/')
+ 
+     return render(request, 'compras/index.html')         
     
 
-def salir(request):
-   logout(request)
-   messages.info(request,"Sesion Terminada")
-   return redirect("Inicio")
+def logout_user(request):
+    logout(request)
+    messages.success(request,("Haz Finalizado Sesion "))
+    return redirect('/')
 
 
 
 def compras(request):
-    form = Producto.objects.all()
-    return render(request,"compras/compras.html",{'form':form})
+    if request.user.is_authenticated:
+        form = Producto.objects.all()
+        return render(request,"compras/compras.html",{'form':form})
+    else:
+        return redirect('/')    
 
 
 def nuevo(request):
-    form = FormProducto()
-    if request.method == "POST":
-        form = FormProducto(request.POST)
+    if request.user.is_authenticated:
+        form = FormProducto()
+        if request.method == "POST":
+            form = FormProducto(request.POST)
      
-        if form.is_valid():
-          try:
-              data = Producto()#tabla a guarda los datos
-              data.nombre = form.cleaned_data['nombre']
-              data.categoria = form.cleaned_data['categoria']
-              data.cantidad = form.cleaned_data['cantidad']
-              data.precio = form.cleaned_data['precio']
-              data.save()
-              return redirect('Nuevo')
-          except:
-              return redirect('compras/error.html')
-
-
-    return render(request,"compras/nuevo.html",{'form':form})
-
-
+            if form.is_valid():
+              try:
+                data = Producto()#tabla a guarda los datos
+                data.nombre = form.cleaned_data['nombre']
+                data.categoria = form.cleaned_data['categoria']
+                data.cantidad = form.cleaned_data['cantidad']
+                data.precio = form.cleaned_data['precio']
+                data.save()
+                return redirect('Nuevo')
+              except:
+                return redirect('compras/error.html')
+        return render(request,"compras/nuevo.html",{'form':form})
+    else:
+        return redirect('/')
 
 
 def editar(request,id):
-    producto = Producto.objects.get(id = id)
-    if request.method == 'GET':
-        form = FormProducto(instance=producto)
-    else:
-        form = FormProducto(request.POST,instance = producto)
+    if request.user.is_authenticated:
+        producto = Producto.objects.get(id = id)
+        if request.method == 'GET':
+            form = FormProducto(instance=producto)
+        else:
+            form = FormProducto(request.POST,instance = producto)
      
-        if form.is_valid():
-          try:
-              form.save()
-              return redirect('Compras')#la redireccion es en name del path en la url  path('',views.compras,name="Compras"),
-          except:
-              return redirect('Error') # path('error/',views.error,name="Error"),
+            if form.is_valid():
+                try:
+                    form.save()
+                    return redirect('Compras')#la redireccion es en name del path en la url  path('',views.compras,name="Compras"),
+                except:
+                    return redirect('Error') # path('error/',views.error,name="Error"),
 
-    return render(request,"compras/editar.html",{'form':form})
+        return render(request,"compras/editar.html",{'form':form})
+    else:
+        return redirect('/')    
 
 
 
 def eliminar(request,id):
+    if request.user.is_authenticated:
         try:
             producto = Producto.objects.get(id = id)
             producto.delete()
             return redirect('Compras')#la redireccion es en name del path en la url  path('',views.compras,name="Compras"),
         except:
             return redirect('Error') # path('error/',views.error,name="Error"),
-   
+    else:
+        return redirect('/')
 
 
 
 def error(request):
-    return render(request,'error.html')    
+    return render(request,'compras/error.html')    
